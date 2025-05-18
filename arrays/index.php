a0ff34dbee012389733af5067869a030ad03ba2b
M-M-I-V <neiljaylacandazo@gmail.com>
2025-05-18 11:57:18 +0800

    Added index.php

---
+++ arrays/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $foods = array("Pizza", "Burger", "Pasta", "Salad", "Sushi");
+
+    echo $foods[0] . "<br>";
+    echo $foods[1] . "<br>";
+    echo $foods[2] . "<br>";
+    echo $foods[3] . "<br>";
+    echo $foods[4];
+
+?>
