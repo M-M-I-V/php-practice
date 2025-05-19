018534842e34bb9b5053bd839385e912bf40351a
M-M-I-V <neiljaylacandazo@gmail.com>
2025-05-19 10:45:56 +0800

    Added index.php

---
+++ str-functions/index.php
@@ -0,0 +1,32 @@
+<?php
+
+    $username = "mmiv";
+    $name = "Neil Jay Lacandazo";
+    $nameInArray = array("Neil Jay", "Lacandazo");
+
+    /* $username = strtolower($username);
+    $username = strtoupper($username);
+    $username = trim($username);
+    $username = str_pad($username, 10, "$");
+    $name = str_replace(" ", "-", $name);
+    $username = strrev($username);
+    $username = str_shuffle($username);
+    $compare = strcmp($username, "mmiv");
+    $count = strlen($username);
+    $index = strpos($name, " ");
+    $firstName = substr($name, 0, 7);
+    $lastName = substr($name, 8);
+    $sepName = explode(" ", $name);
+    $fullName = implode(" ", $nameInArray); */
+    
+    echo $username . "<br>";
+    echo $name . "<br>";
+    echo $compare . "<br>";
+    echo $count . "<br>";
+    echo $index . "<br>";
+    echo $firstName . "<br>";
+    echo $lastName . "<br>";
+    echo $sepName . "<br>";
+    echo $fullName . "<br>";
+    
+?>
