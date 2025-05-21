7872f7c69bb07847468b3fc0706a9c6bffe7d3a6
M-M-I-V <neiljaylacandazo@gmail.com>
2025-05-21 11:56:27 +0800

    Updated index.php

--- php-mysql-query/index.php
+++ php-mysql-query/index.php
@@ -6,13 +6,13 @@
     <title>SQL query using PHP</title>
 </head>
 <body>
-    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
         <h3>Register</h3>
         <label>username</label>
-        <input type="text" name="username">
+        <input type="text" name="username"><br>
         <label>password</label>
-        <input type="password" name="password">
-        <input type="submit" name="submit" value="Submit">
+        <input type="password" name="password"><br>
+        <input type="submit" name="submit" value="Submit"><br><br>
     </form>
 </body>
 </html>
@@ -20,20 +20,38 @@
 <?php
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $db_server = "localhost";
+        $db_user = "root";
+        $db_password = "";
+        $db_name = "business";
+        $db_connection = "";
+
+        try {
+            $db_connection = mysqli_connect($db_server, $db_user, $db_password, $db_name);
+            echo "Connected!<br>";
+
+        } catch(mysqli_sql_exception) {
+            echo "Can't connect to the database.";
+
+        }
+
         $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
         $inputtedPassword = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
         $password = password_hash($inputtedPassword, PASSWORD_DEFAULT);
 
-        include("../mysqli/database.php");
-
-        $query = "INSERT INTO users(user, password) VALUES('$username', '$password')";
+        $stmt = mysqli_prepare($db_connection, "INSERT INTO users(username, password) VALUES(?, ?)");
+        mysqli_stmt_bind_param($stmt, "ss", $username, $password);
         
         try {
-            mysqli_query($connection, $query);
+            mysqli_stmt_execute($stmt);
+            echo "Successful! User is now registered.";
 
-        } catch(mysqli_sql_exception) {
-            echo "Error!";
+        } catch(mysqli_sql_exception $e) {
+            echo "Error! Couldn't register user: " . $e->getMessage();
 
+        } finally {
+            mysqli_stmt_close($stmt);
+            mysqli_close($db_connection);
         }
     }
 
